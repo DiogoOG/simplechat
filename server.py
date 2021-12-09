54f2cc69c15df7b210569916dbda633e4e70234d
Diog <diogogouveia00@hotmail.com>
2021-12-09 21:11:14 +0000

    Argumentos em linha de comandos
    Para correr, usar um argumento do género:
    0.0.0.0:9000

--- server.py
+++ server.py
@@ -1,36 +1,31 @@
 import socket
 from threading import Thread
-import datetime
+import sys
 
-MAX_CONNECTIONS =  5
-BUFFER_SIZE = 1024 # razoável me thinks
+MAX_CONNECTIONS =  10
+BUFFER_SIZE = 1024
 
 client_sockets = []
 host_socket = socket.socket()
 
-# Para pedir informação para conexão
 def connect():
-    valid = False
-    while not valid:
-        try:
-            HOST, PORT = input("Host IP: ").split(':')
-            PORT = int(PORT)
-        
-            host_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-            host_socket.bind((HOST,PORT))
+    if len(sys.argv) < 2:
+        raise Exception("Poucos argumentos (usar 'server.py IP:PORT').")
 
-            valid = True
-        except Exception as e:
-            print(e)
+    IP, PORT = sys.argv[1].split(':')
 
-    print(f"Ouvindo como {HOST}:{PORT}.")
+    PORT = int(PORT)
 
-    # Ativar o socket para MAX_CONNECTIONS ligações vindouras
+    host_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    host_socket.bind((IP,PORT))
+
+    print(f"Ouvindo como {IP}:{PORT}.")
+
     host_socket.listen(MAX_CONNECTIONS)
-
-# Aqui a função para lidar com uma conexão
-def handle(client_socket, client_address):
+    
+def handle(client_socket,client_address):
     client_sockets.append(client_socket)
     while True:
         try:
@@ -48,58 +43,30 @@ def handle(client_socket, client_address):
                     except Exception:
                         pass
 
-# Com a função definida, criar tarefas (threads) para cada uma das conexões recebidas
-
-# Código da tarefa para esperar por conexões
 def listener():
     while True:
         client_socket, client_address = host_socket.accept()
 
-        client_thread = Thread ( target=handle, args=(client_socket, client_address,) )
+        client_thread = Thread ( target=handle, args=(client_socket,client_address,) )
 
         client_thread.daemon = True
 
         client_thread.start()
 
-# Iniciar a tarefa
-def listen(listener_thread):
+def start_listening(listener_thread):
     listener = Thread ( target = listener_thread )
     listener.daemon = True
     listener.start()
 
-# Comandos simples
-def getCommands():
-
-    commands = ["quit","help","broadcast"]
-
-    while True:
-        com = input(" > ").split(' ')
-        if (com[0] not in commands):
-            print("Comando não reconhecido.")
-        elif (com[0] == "quit"):
-            for cs in client_sockets:
-                try:
-                    cs.send( "Servidor encerrado".encode() )
-                    cs.close()
-                except Exception:
-                    pass
-            return
-        elif(com [0] == "broadcast"):
-            if len(com) < 2:
-                print("Insira uma mensagem após broadcast!")
-            else:
-                msg = ""
-                for word in com[1:]:
-                    msg = msg + word
-                for cs in client_sockets:
-                    try:
-                        cs.send( (f"[{datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] SERVIDOR > {msg}").encode() )
-                    except Exception:
-                        pass
-        elif(com [0] == "/help"):
-            print(commands)
-
-# Agora basta apenas correr
 connect()
-listen(listener)
-getCommands()
+start_listening(listener)
+
+while True:
+    com = input()
+    if com == "CLOSE":
+        for cs in client_sockets:
+            try:
+                cs.send( "Servidor encerrado".encode() )
+                cs.close()
+            except Exception:
+                pass
